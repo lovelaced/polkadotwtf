c8d1b18cd734dcf8139e040a0469633e3517833f
Erin Grasmick <erin@parity.io>
2024-10-15 19:09:40 +0100

    affix footer for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ export const Footer = () => (
       <a href="mailto:erin@parity.io">Feedback</a>
     </div>
     <div className="footer-button">
-      <a href="https://github.com/lovelaced/polkadotwtf/issues/new?assignees=lovelaced&labels=enhancement&projects=&template=add_chain.md&title=Add+support+for+%5BBlockchain+Name%5D">Add Chain</a>
+      <a href="https://github.com/lovelaced/polkadotwtf/issues/new?assignees=lovelaced&labels=enhancement&projects=&template=add_chain.md&title=Add+support+for+%5BBlockchain+Name%5D" target="_blank" rel="noopener noreferrer">Add Chain</a>
     </div>
     <div className="footer-button">
-      <a href="https://github.com/lovelaced/polkadotwtf">Source Code</a>
+      <a href="https://github.com/lovelaced/polkadotwtf" target="_blank" rel="noopener noreferrer">Source Code</a>
     </div>
     <div className="footer-text">polkadot.wtf - built with ❤</div>
   </div>
