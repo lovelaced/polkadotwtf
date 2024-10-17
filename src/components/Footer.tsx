dddf140bd63f74df8c6e54327eff404bef76e7df
Erin Grasmick <erin@parity.io>
2024-10-17 10:27:34 +0100

    button fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,8 @@
 export const Footer = () => (
   <div className="footer">
-    <div className="footer-button">
-      <a href="mailto:erin@parity.io">Feedback</a>
-    </div>
-    <div className="footer-button">
-      <a href="https://github.com/lovelaced/polkadotwtf/issues/new?assignees=lovelaced&labels=enhancement&projects=&template=add_chain.md&title=Add+support+for+%5BBlockchain+Name%5D" target="_blank" rel="noopener noreferrer">Add Chain</a>
-    </div>
-    <div className="footer-button">
-      <a href="https://github.com/lovelaced/polkadotwtf" target="_blank" rel="noopener noreferrer">Source Code</a>
-    </div>
+      <a href="mailto:erin@parity.io" className="footer-button">Feedback</a>
+      <a href="https://github.com/lovelaced/polkadotwtf/issues/new?assignees=lovelaced&labels=enhancement&projects=&template=add_chain.md&title=Add+support+for+%5BBlockchain+Name%5D" target="_blank" rel="noopener noreferrer" className="footer-button">Add Chain</a>
+      <a href="https://github.com/lovelaced/polkadotwtf" target="_blank" rel="noopener noreferrer" className="footer-button">Source Code</a>
     <div className="footer-text">polkadot.wtf - built with ❤</div>
   </div>
 );
